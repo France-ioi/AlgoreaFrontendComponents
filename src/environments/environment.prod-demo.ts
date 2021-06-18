a0335cea9ef8a705d6caa09195215880252dd219
Michel Blockelet <blockelet@crans.org>
2021-06-18 16:42:45 +0100

    Add production-demo configuration

---
+++ src/environments/environment.prod-demo.ts
@@ -0,0 +1,20 @@
+import { Environment } from 'src/app/shared/helpers/config';
+
+export const environment: Environment = {
+  production: true,
+  apiUrl: 'https://dev.algorea.org/api',
+  oauthServerUrl: 'https://login.france-ioi.org',
+  oauthClientId: '43',
+
+  defaultActivityId: '1352246428241737349',
+  defaultSkillId: '3000',
+
+  languages: [
+    { tag: 'fr', path: '/' },
+    { tag: 'en', path: '/en/' },
+  ],
+  allowForcedToken: true,
+  authType: 'cookies',
+  secure: true,
+  sameSite: false,
+};
